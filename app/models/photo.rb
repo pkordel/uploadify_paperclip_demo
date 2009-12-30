5b596c154235de528087dc6debd269dbeacd7313
Peter Kordel <pkordel@gmail.com>
2009-12-30 17:09:46 +0100

    Added s3 support

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :thumb => "100x100>" }
-
+  has_attached_file :photo, 
+                    :styles => { :thumb => "100x100>" },
+                    :path => "images/products/:id/:style/:basename.:extension",
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml"
 end
