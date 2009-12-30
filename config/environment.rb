2703def9c661236097eb2d52e2d2572b33d522e7
Peter Kordel <pkordel@gmail.com>
2009-12-30 20:50:04 +0100

    Removed secret key stuff

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'paperclip', :source => 'http://gemcutter.org'
   config.gem 'mime-types',                  :lib => "mime/types",     :version => '1.16'
-  config.gem 'uuidtools', :version => '>= 2.1.1'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
