0f49383cdd0a75c12233eaa0f75d798150001d49
Peter Kordel <pkordel@gmail.com>
2010-05-22 11:13:24 +0200

    updated RAEDME and made preview optional

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,6 @@
 Paperclip.interpolates(:s3_authenticated_url) do |attachment, style| 
   AWS::S3::S3Object.url_for(attachment.path(style), attachment.bucket_name, :use_ssl=>true, :expires_in=>3.minutes) 
 end 
+
+# Change this for heroku, e.g. ":rails_root/tmp/uploads"
+TEMP_PATH_PREFIX = ":rails_root/public/uploads"
