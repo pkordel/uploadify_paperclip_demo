b76060d3809c625a3c8b709f99bb9db6b736104c
Peter Kordel <pkordel@gmail.com>
2010-05-22 09:41:42 +0200

    support for styles

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   has_attached_file :photo, 
-                    :path => "#{S3_CONFIG['key']}/:id/:basename.:extension",
+                    :styles => { :mini => '48x48#', :small => '178x92>', :product => '393x203>' }, 
+                    :default_style => :product,
+                    :path => "#{S3_CONFIG['key']}/:id/:style/:basename.:extension",
                     :storage => :s3,
                     :s3_credentials => "#{RAILS_ROOT}/config/amazon_s3.yml", 
                     :s3_permissions=>'private', :url=>':s3_authenticated_url'
--- app/models/temp_photo.rb
+++ app/models/temp_photo.rb
@@ -1,9 +1,11 @@
 class TempPhoto < Photo
   has_attached_file :photo,
-                    :path => ":rails_root/tmp/uploads/:id/:basename.:extension" # Save it to a temporay location
+                    :styles => { :mini => '48x48#', :small => '178x92>', :product => '393x203>' }, 
+                    :default_style => :product,
+                    :path => ":rails_root/tmp/uploads/:id/:style/:basename.:extension" # Save it to a temporay location
                     
   # Save this as a regular upload on s3
-  # this is ran after the file is saved
+  # this is run after the file is saved
   # and all post process stuff like
   # resizing is done
   after_save :queue_move_to_s3
@@ -15,17 +17,19 @@ class TempPhoto < Photo
   end
 
   def move_to_s3
-    temp_path = photo.path
-    temp_file = photo.to_file # Paperclips way of getting a File object for the attachment
+    [:mini, :small, :product, :original].each do |style|
+      temp_path = photo.path(style)
+      temp_file = photo.to_file(style) # Paperclip's way of getting a File object for the attachment
 
-    # Save it as a regular attachment
-    # this will save to S3
-    s3_upload = Photo.find(id) # Same db record but we need the S3 version
-    s3_upload.photo = temp_file  # reset the file - it will assume its a new file
-    s3_upload.save!             # Paperclip will upload the file on save
+      # Save it as a regular attachment
+      # this will save to S3
+      s3_upload = Photo.find(id) # Same db record but we need the S3 version
+      s3_upload.photo = temp_file  # reset the file - it will assume its a new file
+      s3_upload.save!             # Paperclip will upload the file on save
 
-    # Delete the temporary file when we are done
-    temp_file.close
-    File.delete(temp_path)
+      # Delete the temporary file when we are done
+      temp_file.close
+      File.delete(temp_path)
+    end
   end
 end
